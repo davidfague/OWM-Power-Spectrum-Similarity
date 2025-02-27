44ba6369c4ad7d850bef7c5382a5613961d1565d
Fague <drfrbc@umsystem.edu>
2025-02-27 11:38:28 -0600

    implmenet some method for picking band boundaries in check_psvs...m

--- scripts/check_psvs_for_freq_bands.m
+++ scripts/check_psvs_for_freq_bands.m
@@ -52,7 +52,7 @@ disp(unique(all_label_table.session_ID));
 save("all_label_table.mat", "all_label_table");
 %% gather PSVs for this anat across patients.
 % problem: image_id -> image is different across patients.
-es_freq_band = [8:30];
+es_freq_band = [1:40];
 % do all images first
 possible_PSVs = nan([681, length(es_freq_band), sum(sum(all_label_table.encoding_correctness(:,:), 2)==3)]);
 
@@ -103,12 +103,78 @@ for k12wm = [true, false]
         end
     end
 end
+
+%% plot
 plot_params.frequencies_to_use = [];
 plot_params.anat = "All-Anat";
 title_str = sprintf("Correct-Trials All-Patients %d-%d", min(es_freq_band), max(es_freq_band));
 plot_params.enc_window_ids = params.enc1_win_IDs;
 enforced_clim = [];
-fig = plot_PSV(mean(possible_PSVs(:,params.maint_win_IDs,:), [2 3], 'omitnan'), plot_params, title_str, enforced_clim);
+% plot whole-trial mean across all
+% fig = plot_PSV(mean(possible_PSVs, 3, 'omitnan'), plot_params, title_str, enforced_clim);
+%%
+plot = false;
+total_aboves = 0;
+total_belows = 0;
+% clean up nans
+rows_to_use = all(~isnan(possible_PSVs),[1 2]);
+all_label_table = all_label_table(rows_to_use,:);
+possible_PSVs = possible_PSVs(:,:,rows_to_use);
+
+% % plot mean maintenance all obs
+% limits_to_use = get_clim_based_on_2z(mean(possible_PSVs(params.maint_win_IDs,:,:),1, 'omitnan'));
+% imagesc(squeeze(mean(possible_PSVs(params.maint_win_IDs,:,:),1, 'omitnan')));
+% clim(limits_to_use)
+
+% plot mean maintenance, subset each region
+all_label_table.anat = string(all_label_table.anatomical_label);
+unique_anats = unique(all_label_table.anat);
+for anat_idx = 1:length(unique_anats)
+    anat = unique_anats(anat_idx);
+
+    rows_to_use = strcmp(all_label_table.anat, (anat));
+    if plot
+        limits_to_use = get_clim_based_on_2z(mean(possible_PSVs(params.maint_win_IDs,:,rows_to_use),1, 'omitnan'));
+        fig = figure;
+        imagesc(squeeze(mean(possible_PSVs(params.maint_win_IDs,:,rows_to_use),1, 'omitnan')));
+        clim(limits_to_use)
+        title( sprintf("Z power spectra\nAll-Patients\nCorrect-Trials\n%d-%d Hz\n%s", min(es_freq_band), max(es_freq_band), anat));
+        ylabel("frequency (Hz)")
+        xlabel("observations (channel, trial)")
+        save_dir = fullfile(sprintf("results/Z power spectra All-Patients Correct-Trials %d-%d Hz by anat/", min(es_freq_band), max(es_freq_band)));
+        if ~exist(save_dir, 'dir')
+            mkdir(save_dir)
+        end
+        savefig(fig, fullfile(sprintf("%s/%s", save_dir, anat)))
+    end
+
+    band_btwn = 12:14;
+    band_above = 15:17;
+    band_below = 9:11;
+    closer_to_str = mean_power_is_closer_to(band_btwn, band_above, band_below, possible_PSVs(params.maint_win_IDs,:,rows_to_use));
+    if strcmp(closer_to_str, "above")
+        total_aboves = total_aboves+1;
+    else
+        total_belows=total_belows+1;
+    end
+    fprintf("%s is closer to band %s\n", anat, closer_to_str)
+
+end
+
+function closer_to_str = mean_power_is_closer_to(band_btwn, band_above, band_below, PSVs)
+    mean_band_btwn = mean(PSVs(:,band_btwn,:), [1:3]);
+    mean_band_above = mean(PSVs(:,band_above,:), [1:3]);
+    mean_band_below = mean(PSVs(:,band_below,:), [1:3]);
+    
+    dist_below = abs(mean_band_btwn - mean_band_below);
+    dist_above = abs(mean_band_btwn - mean_band_above);
+    
+    if dist_below > dist_above
+        closer_to_str = 'above';
+    else
+        closer_to_str = 'below';
+    end
+end
 
 %% this is channel-specific method (not used here)
 
