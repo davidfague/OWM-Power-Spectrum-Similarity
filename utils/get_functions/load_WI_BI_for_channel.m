84019d69bfe23125aeef73d83a093eb424aa1de4
Fague <drfrbc@umsystem.edu>
2025-03-10 10:14:23 -0500

    better error handling for when the channel map is missing.

--- utils/get_functions/load_WI_BI_for_channel.m
+++ utils/get_functions/load_WI_BI_for_channel.m
@@ -1,20 +1,22 @@
 function [EMS_WI, EMS_BI] = load_WI_BI_for_channel(params)
     [WI_channel_map] = load_BT_channel_map(params, 'WI');
-    if isempty(WI_channel_map)
+    try
+        WI = WI_channel_map(params.chan_id);
+    catch ME
+        warning('chan %s is not in WI_channel_map', params.chan_id)
         EMS_WI = [];
         EMS_BI = [];
         return
-    else
-        WI = WI_channel_map(params.chan_id);
     end
     clear WI_channel_map
     [BI_channel_map] = load_BT_channel_map(params, 'BI');
-    if isempty(BI_channel_map)
+    try
+        BI = BI_channel_map(params.chan_id);
+    catch ME
+        warning('chan %s is not in BI_channel_map', params.chan_id)
         EMS_WI = [];
         EMS_BI = [];
         return
-    else
-        BI = BI_channel_map(params.chan_id);
     end
     clear BI_channel_map
     [EMS_WI, EMS_BI] = pre_process_WI_BI(params, WI, BI);
